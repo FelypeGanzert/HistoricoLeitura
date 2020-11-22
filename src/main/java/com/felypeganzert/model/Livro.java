f1c931e19924f6b0b2477f4d0e5f10c687440710
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2020-11-22 03:42:12 -0300

    Criado o modelo Livro

---
+++ src/main/java/com/felypeganzert/model/Livro.java
@@ -0,0 +1,127 @@
+package com.felypeganzert.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Livro implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String titulo;
+	private String autor;
+	private Date dataInicio;
+	private Date dataTermino;
+	private Integer paginas;
+	private Integer paginasLidas;
+	private String urlCapa;
+	
+	public Livro() {
+	}
+	
+	public Livro(String titulo, String autor) {
+		this.titulo = titulo;
+		this.autor = autor;
+	}
+
+	public Livro(Integer id, String titulo, String autor, Date dataInicio, Date dataTermino,
+			Integer paginas, Integer paginasLidas, String urlCapa) {
+		this.id = id;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.dataInicio = dataInicio;
+		this.dataTermino = dataTermino;
+		this.paginas = paginas;
+		this.paginasLidas = paginasLidas;
+		this.urlCapa = urlCapa;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Date getDataTermino() {
+		return dataTermino;
+	}
+
+	public void setDataTermino(Date dataTermino) {
+		this.dataTermino = dataTermino;
+	}
+
+	public Integer getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(Integer paginas) {
+		this.paginas = paginas;
+	}
+
+	public Integer getPaginasLidas() {
+		return paginasLidas;
+	}
+
+	public void setPaginasLidas(Integer paginasLidas) {
+		this.paginasLidas = paginasLidas;
+	}
+
+	public String getUrlCapa() {
+		return urlCapa;
+	}
+
+	public void setUrlCapa(String urlCapa) {
+		this.urlCapa = urlCapa;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
